8d04fead152c841e28f87970a89bfda34a8a3404
Weslei Teixeira da Silveira <wesleiteixeira@hotmail.com.br>
2018-12-10 23:07:24 +0000

    scripts updated

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -91,11 +91,4 @@ if ( !defined('ABSPATH') )
 	define('ABSPATH', dirname(__FILE__) . '/');
 
 /** Configura as variáveis e arquivos do WordPress. */
-// AWS ALB REVERSE PROXY CONFIGURATION
-define('FORCE_SSL_ADMIN', true);
-if (strpos($_SERVER['HTTP_X_FORWARDED_PROTO'], 'https') !== false)
-{ 
-  $_SERVER['HTTPS']='on';
-}
-
 require_once(ABSPATH . 'wp-settings.php');
